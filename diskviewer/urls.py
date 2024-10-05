738690acf415b2a70ea706f370e17bd89b7d840c
g.rahimzhanov <cubicscience@yandex.ru>
2024-10-05 15:54:51 +0300

    Implement public key input form and file list display

--- diskviewer/urls.py
+++ diskviewer/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('files/', views.file_list, name='file_list'),
+    path('download/', views.download_file, name='download_file'),
 ]
--- diskviewer/views.py
+++ diskviewer/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.http import (HttpResponseRedirect, HttpResponse)
+from django.shortcuts import (render, redirect)
+import requests
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'diskviewer/index.html')
 
 
-def index():
-    pass
+def file_list(request):
+    if request.method == 'POST':
+        public_key = request.POST.get('public_key')
+        api_url = 'https://cloud-api.yandex.net/v1/disk/public/resources'
+        params = {'public_key': public_key}
+        response = requests.get(api_url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            items = data['_embedded']['items']
+            return render(request, 'diskviewer/file_list.html', {'items': items, 'public_key': public_key})
+        else:
+            return HttpResponse("Ошибка при получении данных с Яндекс.Диска")
+    else:
+        return redirect('index')
+
+
+def download_file(request):
+    public_key = request.GET.get('public_key')
+    path = request.GET.get('path')
+    api_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download'
+    params = {'public_key': public_key, 'path': path}
+    response = requests.get(api_url, params=params)
+    if response.status_code == 200:
+        download_url = response.json()['href']
+        return HttpResponseRedirect(download_url)
+    else:
+        return HttpResponse("Ошибка при скачивании файла")
